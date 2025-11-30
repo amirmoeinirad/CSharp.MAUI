cbce22e4e4770d20390f7ec6b2886bc6f1ec4b14
Amir Moeini Rad <moeini@shabakehafzar.com>
2025-11-30 13:04:18 +0330

    Add project files.

---
+++ NotesApp/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NotesApp;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ NotesApp/Models/Note.cs
@@ -0,0 +1,20 @@
+﻿using SQLite;
+
+namespace NotesApp.Models
+{
+    // This class is a data model.
+    // A model in MAUI represents the data used in the application by Services and ViewModels.
+    // Here, the model is mapped to a SQLite database.
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        [MaxLength(250)]
+        public string Title { get; set; } = string.Empty;
+
+        public string Body { get; set; } = string.Empty;
+
+        public DateTime LastModified { get; set; } = DateTime.UtcNow;
+    }
+}
